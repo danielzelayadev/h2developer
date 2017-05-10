55f2dae7d18c329d5c31606ee451cba18f754555
wupa9 <wupaz9@gmail.com>
2017-05-09 19:01:43 -0600

    Inject connection service and call newConnection on form submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Connection } from './domain/connection';
+import { ConnectionService } from './connection.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   displayConnectionModal = false;
 
-  onNewConnectionSubmit(conn : Connection) {
+  constructor(private connService : ConnectionService){}
+
+  ngOnInit() {}
+
+  async onNewConnectionSubmit(conn : Connection) {
     this.displayConnectionModal = false;
+    try {
+      const newConn = await this.connService.newConnection(conn);
+      console.log("New Connection:", newConn);
+    } catch (err) {
+      console.error(err);
+    }
   }
 
 }
