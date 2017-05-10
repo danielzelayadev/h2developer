e180b7312a21bc2f436144c1db85a9168b0b3b32
wupa9 <wupaz9@gmail.com>
2017-05-10 01:49:57 -0600

    Get connections, init tree, and setup growls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { Connection } from './domain/connection';
 import { ConnectionService } from './connection.service';
+import { UtilsService } from './utils.service';
+
+import { Message, TreeNode } from 'primeng/primeng';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +14,49 @@ import { ConnectionService } from './connection.service';
 export class AppComponent implements OnInit {
   displayConnectionModal = false;
 
-  constructor(private connService : ConnectionService){}
+  connections : string[] = [];
 
-  ngOnInit() {}
+  tree : TreeNode[] = [];
+  treeStyles = { height: '93.6%' };
+
+  msgs : Message[] = [];
+
+  constructor(
+    private connService : ConnectionService,
+    private utils : UtilsService){}
+
+  ngOnInit() {
+    this.getConnections();
+  }
+
+  async getConnections() {
+    try {
+      const conns = await this.connService.getConnections();
+      conns.map(this.pushNewConn.bind(this));
+    } catch (errMsg) {
+      this.msgs.push(this.utils.error("Failed to Load Connections", errMsg));
+    }
+  }
 
   async onNewConnectionSubmit(conn : Connection) {
     this.displayConnectionModal = false;
     try {
       const newConn = await this.connService.newConnection(conn);
-      console.log("New Connection:", newConn);
-    } catch (err) {
-      console.error(err);
+      this.pushNewConn(newConn.url);
+      this.msgs.push(this.utils.success('Success', 'Connection added to H2Developer.'));
+    } catch (errMsg) {
+      this.msgs.push(this.utils.error('New Connection Failed', errMsg));
     }
+  }
+
+  pushNewConn(label : string) {
+    this.tree.push({
+      label, data: { isConn: true, label },
+      icon: "fa-database", leaf: false
+    })
+  }
+
+  onNodeExpand(e) {
   }
 
 }
