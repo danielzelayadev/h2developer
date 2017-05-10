f8508ca4d6cd67a0b284505d6565bd06011999f5
wupa9 <wupaz9@gmail.com>
2017-05-10 01:32:38 -0600

    Get connections and handle errors

--- src/app/connection.service.ts
+++ src/app/connection.service.ts
@@ -13,14 +13,28 @@ export class ConnectionService {
 
   constructor(private http : Http) { }
 
+  async getConnections() : Promise<string[]> {
+    try {
+      const response = await this.http.get(this.url).toPromise();
+      return response.json().map(e => e.url);
+    } catch(e) {
+      return this.handleError(e);
+    }
+  }
+
   async newConnection(conn : Connection) : Promise<Connection> {
     try {
-      const response = await this.http.post(`${this.url}`, conn).toPromise();
-      console.log(response);
+      const response = await this.http.post(this.url, conn).toPromise();
       return response.json();
     } catch (e) {
-      console.error(e);
+      return this.handleError(e);
     }
+  }
+
+  private handleError(e : any) : Promise<any> {
+    const error = JSON.parse(e._body);
+    console.error('Server says:\n', error.log);
+    return Promise.reject(error.message);
   }
 
 }
