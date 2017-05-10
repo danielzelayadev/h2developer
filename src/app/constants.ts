da8c6c99f6da10bbd4c7b8dcef09cc6c8af78ac5
wupa9 <wupaz9@gmail.com>
2017-05-09 18:58:33 -0600

    Add app constants

---
+++ src/app/constants.ts
@@ -0,0 +1,4 @@
+export const SERVER_PORT : number = 8080;
+export const PROTOCOL : string = 'http';
+export const SERVER : string = 'localhost';
+export const SERVER_URL : string = `${PROTOCOL}://${SERVER}:${SERVER_PORT}`;
