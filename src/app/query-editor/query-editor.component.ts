a996bf5b4f206f5b2547f95b2c58aba1bd7a8d3b
wupa9 <wupaz9@gmail.com>
2017-05-11 13:28:24 -0600

    Add input for disabled and event emitter for ctrl enter

--- src/app/query-editor/query-editor.component.ts
+++ src/app/query-editor/query-editor.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'query-editor',
   templateUrl: './query-editor.component.html',
   styleUrls: ['./query-editor.component.scss']
 })
-export class QueryEditorComponent implements OnInit {
+export class QueryEditorComponent {
 
-  constructor() { }
+  @Input() disabled = false;
+  @Output() onCtrlEnter = new EventEmitter<string[]>();
 
-  ngOnInit() {
+  text : string = '';
+
+  ctrlEnterPressed() {
+    this.onCtrlEnter.emit(this.text.split(/[.]+;/igm));
   }
 
 }
