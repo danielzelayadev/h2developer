8643888272561df4de0fc807bcbf5777a9c7d775
wupa9 <wupaz9@gmail.com>
2017-05-13 11:44:56 -0600

    Setup two-way data biding for editor

--- src/app/query-editor/query-editor.component.ts
+++ src/app/query-editor/query-editor.component.ts
@@ -8,12 +8,27 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class QueryEditorComponent {
 
   @Input() disabled = false;
+  @Output() textChange = new EventEmitter<string>();
   @Output() onCtrlEnter = new EventEmitter<string>();
 
-  text : string = '';
+  queryVal : string = "";
+
+  @Input()
+  get text() : string {
+    return this.queryVal;
+  }
+
+  set text(val : string) {
+    this.queryVal = val;
+    this.textChange.emit(this.queryVal);
+  }
 
   ctrlEnterPressed() {
-    this.onCtrlEnter.emit(this.text);
+    this.onCtrlEnter.emit(this.queryVal);
+  }
+
+  keypressed(e) {
+    this.text = e.target.value;
   }
 
 }
